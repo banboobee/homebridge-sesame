430f6790660fe6b505ae77e850f54cbcfe4b7549
banboobee <qzh00033@nifty.ne.jp>
2022-02-18 06:04:46 +0900

    Missed index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
+// -*- mode : js; js-indent-level : 2 -*-
+import { API } from 'homebridge';
 import { LockPlatform } from './src/LockPlatform';
 import { HAP } from './src/HAP';
 
-export = (homebridge) => {
+export = (homebridge: API) => {
   HAP.Accessory = homebridge.platformAccessory;
   HAP.Service = homebridge.hap.Service;
   HAP.Characteristic = homebridge.hap.Characteristic;
   HAP.UUID = homebridge.hap.uuid;
+  HAP.User = homebridge.user;
 
-  homebridge.registerPlatform('homebridge-sesame', 'Sesame', LockPlatform, true);
+  homebridge.registerPlatform('homebridge-sesame', 'Sesame', LockPlatform);
 }
